28b29a7aacd22a09b9c81240845471802086990e
aviadamd <aviadamd@gmail.com>
2022-01-08 15:34:37 +0200

    fix

--- src/main/java/base/driverManager/Manager.java
+++
@@ -1,8 +0,0 @@
-package base.driverManager;
-
-import org.openqa.selenium.WebDriver;
-
-public interface Manager {
-    WebDriver getDriver();
-    void quitDriver();
-}
--- src/main/java/base/driverManager/PlatformsType.java
+++
@@ -1,8 +0,0 @@
-package base.driverManager;
-
-public interface PlatformsType {
-    String CHROME = "chrome";
-    String FIREFOX = "firefox";
-    String ANDROID = "android";
-    String REMOTE = "remote";
-}
