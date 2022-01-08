9221e4c8e0e495463c38bfd95289b76348d80f18
aviadamd <aviadamd@gmail.com>
2022-01-08 12:59:22 +0200

    fix

--- src/test/java/test/accountManagement/component/AccountManagementSharedComponents.java
+++ src/test/java/test/accountManagement/component/AccountManagementSharedComponents.java
@@ -14,4 +14,12 @@ public class AccountManagementSharedComponents extends FactoryBaseTest {
         actions.accept(Pair.of(new UiUtilitiesObjects(), new LoginPage(driver)));
         return new AccountManagementSharedComponents();
     }
+
+    public void action(Consumer<Pair<UiUtilitiesObjects, LoginPage>> actions) {
+        try {
+            actions.accept(Pair.of(new UiUtilitiesObjects(), new LoginPage(driver)));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
